8133b8a3badd515ea03dbb9f0cdb08d8e6372950
Nkuna William <NkunaTebogo09@gmail.com>
2022-01-12 00:21:17 +0200

    Added a functionality for downloading and opening the downloaded file

--- app/src/main/java/com/filestorage/FilesStorage.java
+++ app/src/main/java/com/filestorage/FilesStorage.java
@@ -1,16 +1,69 @@
 package com.filestorage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class FilesStorage extends AppCompatActivity {
+
+    private static final String TAG = "FilesStorage";
+
+    private TextView fileNameTextView;
+    private String filepath = "http://www.africau.edu/images/default/sample.pdf";
+    private URL url = null;
+    private String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_files_storage);
 
+        fileNameTextView = findViewById(R.id.fileNameTextView);
 
+        try {
+            url = new URL(filepath);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        fileName = url.getPath();
+        fileName = fileName.substring(fileName.lastIndexOf('/') + 1);
+        fileNameTextView.setText(fileName);
+
+    }
+
+    public void onViewClick(View view) {
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/" + fileName);
+        Uri uri = FileProvider.getUriForFile(FilesStorage.this, "com.filestorage" + ".provider", file);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(uri, "application/pdf");
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivity(intent);
+    }
+
+    public void onDownloadClick(View view) {
+        Log.d(TAG, "onDownloadClick: ");
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url + ""));
+        request.setTitle(fileName);
+        request.setMimeType("application/pdf");
+        request.allowScanningByMediaScanner();
+        request.setAllowedOverMetered(true);
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
+        DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        dm.enqueue(request);
     }
 }
