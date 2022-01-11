3a86defcf39602e097020d86648cab22db39b271
Nkuna William <NkunaTebogo09@gmail.com>
2022-01-11 17:19:14 +0200

    Implemented PDF viewer viewing from assets folder

--- app/src/main/java/com/filestorage/PdfViewer.java
+++ app/src/main/java/com/filestorage/PdfViewer.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class PdfViewer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf_viewer);
+
+        viewPDF();
+    }
+
+    private void viewPDF(){
+        PDFView pdfView = findViewById(R.id.pdfview);
+        pdfView.fromAsset("info.pdf").load();
     }
 }
