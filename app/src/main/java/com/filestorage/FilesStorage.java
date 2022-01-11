9968952b317cda2867e4014d38612bf123e0bd49
Nkuna William <NkunaTebogo09@gmail.com>
2022-01-11 14:42:42 +0200

    bring all project files to github

---
+++ app/src/main/java/com/filestorage/FilesStorage.java
@@ -0,0 +1,16 @@
+package com.filestorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FilesStorage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_files_storage);
+
+
+    }
+}
